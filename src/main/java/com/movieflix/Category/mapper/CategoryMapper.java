258cade1be527f1c3c0bca7998e5b77065f10a9f
Emanuel <fergimanu3@gmail.com>
2025-10-28 22:48:08 -0300

    feat: DTOs creation

---
+++ src/main/java/com/movieflix/Category/mapper/CategoryMapper.java
@@ -0,0 +1,24 @@
+package com.movieflix.category.mapper;
+
+import com.movieflix.category.controller.request.CategoryRequest;
+import com.movieflix.category.controller.response.CategoryResponse;
+import com.movieflix.category.entity.Category;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class CategoryMapper {
+    public static Category toCategory(CategoryRequest categoryRequest){
+        return Category
+                .builder()
+                .name(categoryRequest.name())
+                .build();
+    }
+
+    public static CategoryResponse toCategoryResponse(Category category){
+        return CategoryResponse
+                .builder()
+                .id(category.getId())
+                .name(category.getName())
+                .build();
+    }
+}
