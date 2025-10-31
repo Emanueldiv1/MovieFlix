ee9c663dbf1237f1b117c622a0beb1223bc91868
Emanuel <fergimanu3@gmail.com>
2025-10-31 20:29:47 -0300

    feat: add save and findAll methods

--- src/main/java/com/movieflix/movie/controller/MovieController.java
+++ src/main/java/com/movieflix/movie/controller/MovieController.java
@@ -1,4 +1,36 @@
 package com.movieflix.movie.controller;
 
+import com.movieflix.movie.controller.dtos.request.MoviesRequest;
+import com.movieflix.movie.controller.dtos.response.MovieResponse;
+import com.movieflix.movie.entity.Movie;
+import com.movieflix.movie.mapper.MovieMapper;
+import com.movieflix.movie.service.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movieflix/movie")
+@RequiredArgsConstructor
 public class MovieController {
+
+   private final MovieService movieService;
+
+   @PostMapping("/save")
+   public ResponseEntity<MovieResponse> save(@RequestBody MoviesRequest moviesR){
+      Movie movieSave =  movieService.save(MovieMapper.toMovie(moviesR));
+      return ResponseEntity.ok(MovieMapper.toMovieResponse(movieSave));
+   }
+
+   @GetMapping()
+   public ResponseEntity<List<MovieResponse>> findAll(){
+      return ResponseEntity.ok(movieService.findAll()
+              .stream()
+              .map(movie -> MovieMapper.toMovieResponse(movie))
+              .toList());
+   }
+
+
 }
