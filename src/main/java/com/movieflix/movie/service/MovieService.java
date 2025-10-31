f5f9ea2afdf85df4d8420109d08567ca984546f8
Emanuel <fergimanu3@gmail.com>
2025-10-31 20:33:07 -0300

    feat: implement movie service with save and findAll methods

--- src/main/java/com/movieflix/movie/service/MovieService.java
+++ src/main/java/com/movieflix/movie/service/MovieService.java
@@ -1,4 +1,25 @@
 package com.movieflix.movie.service;
 
+import com.movieflix.movie.entity.Movie;
+import com.movieflix.movie.repository.MovieRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class MovieService {
+
+    private final MovieRepository movieRepository;
+
+    public Movie save(Movie movie){
+       return movieRepository.save(movie);
+    }
+
+    public List<Movie> findAll(){
+        return movieRepository.findAll();
+    }
+
 }
