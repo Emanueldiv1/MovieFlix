12659bbb39ba18e1dd20673c26fde433a9acf16d
Emanuel <fergimanu3@gmail.com>
2025-11-05 23:19:39 -0300

    feat: add repository methods findByCategory and findByStreaming

--- src/main/java/com/movieflix/movie/repository/MovieRepository.java
+++ src/main/java/com/movieflix/movie/repository/MovieRepository.java
@@ -1,9 +1,16 @@
 package com.movieflix.movie.repository;
 
+import com.movieflix.category.entity.Category;
 import com.movieflix.movie.entity.Movie;
+import com.movieflix.streaming.entity.Streaming;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    List<Movie> findMovieByCategories(List<Category> categories);
+    List<Movie> findMovieByStreamings(List<Streaming> streamings);
 }
