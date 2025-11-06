ba5b1f398160571635f26bd9c12a8a4c1a40a8d3
Emanuel <fergimanu3@gmail.com>
2025-11-05 23:24:56 -0300

    feat: implement search endpoint for movies by category and streaming

--- src/main/java/com/movieflix/movie/controller/MovieController.java
+++ src/main/java/com/movieflix/movie/controller/MovieController.java
@@ -1,11 +1,13 @@
 package com.movieflix.movie.controller;
 
+import com.movieflix.category.entity.Category;
 import com.movieflix.movie.controller.dtos.request.MoviesRequest;
 import com.movieflix.movie.controller.dtos.response.MovieResponse;
 import com.movieflix.movie.entity.Movie;
 import com.movieflix.movie.mapper.MovieMapper;
 import com.movieflix.movie.service.MovieService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -49,4 +51,17 @@ public class MovieController {
    }
 
 
+   @GetMapping("/search/by-category")
+   public ResponseEntity<List<MovieResponse>> findByCategory(@RequestParam Long category){
+      return ResponseEntity.ok(movieService.findByCategory(category).stream()
+              .map(movie -> MovieMapper.toMovieResponse(movie))
+              .toList());
+   }
+
+   @GetMapping("/search/by-streaming")
+   public ResponseEntity<List<MovieResponse>> findByStreaming(@RequestParam Long streaming){
+      return ResponseEntity.ok(movieService.findByStreaming(streaming).stream()
+              .map(movie -> MovieMapper.toMovieResponse(movie))
+              .toList());
+   }
 }
