707dd3eb1cf182c934ef508d30a5f05d60584f63
Emanuel <fergimanu3@gmail.com>
2025-11-07 21:16:46 -0300

    feat: implement register method in controller and save in service

--- src/main/java/com/movieflix/user/controller/AuthController.java
+++ src/main/java/com/movieflix/user/controller/AuthController.java
@@ -1,4 +1,30 @@
 package com.movieflix.user.controller;
 
+import com.movieflix.user.controller.request.UserRequest;
+import com.movieflix.user.controller.response.UserResponse;
+import com.movieflix.user.entity.User;
+import com.movieflix.user.mapper.UserMapper;
+import com.movieflix.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("movieflix/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(@RequestBody UserRequest userRequest){
+        User savedUser = userService.register(UserMapper.toUser(userRequest));
+        return ResponseEntity.status(HttpStatus.CREATED).body(UserMapper.toUserReponse(savedUser));
+    }
 }
--- src/main/java/com/movieflix/user/service/UserService.java
+++ src/main/java/com/movieflix/user/service/UserService.java
@@ -1,4 +1,17 @@
 package com.movieflix.user.service;
 
+import com.movieflix.user.entity.User;
+import com.movieflix.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User register(User user){
+        return userRepository.save(user);
+    }
 }
