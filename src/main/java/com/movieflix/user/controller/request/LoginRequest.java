c1a8b79f9db0930a0c4d4ff57151f1c4737dedd5
Emanuel <fergimanu3@gmail.com>
2025-11-10 23:08:36 -0300

    feat: add authentication DTOs

---
+++ src/main/java/com/movieflix/user/controller/request/LoginRequest.java
@@ -0,0 +1,4 @@
+package com.movieflix.user.controller.request;
+
+public record LoginRequest(String email, String password) {
+}
---
+++ src/main/java/com/movieflix/user/controller/response/LoginResponse.java
@@ -0,0 +1,4 @@
+package com.movieflix.user.controller.response;
+
+public record LoginResponse(String token) {
+}
